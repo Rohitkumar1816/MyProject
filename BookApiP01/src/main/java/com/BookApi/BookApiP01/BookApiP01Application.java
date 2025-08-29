ef4a858e93c9c5efc4b03ad7ab560f7c7b6ae6f4
Rohitkumar1816 <rohitkumarbca18@gmail.com>
2025-08-29 17:04:22 +0530

    Add files via upload

---
+++ BookApiP01/src/main/java/com/BookApi/BookApiP01/BookApiP01Application.java
@@ -0,0 +1,13 @@
+package com.BookApi.BookApiP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiP01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiP01Application.class, args);
+	}
+
+}
---
+++ BookApiP01/src/test/java/com/BookApi/BookApiP01/BookApiP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookApi.BookApiP01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
